58beee31c1806e409bbbcc0b1229e88853b89889
Puffbirbb <eaagmhc@students.eaaa.dk>
2022-09-05 12:20:08 +0200

    link attribut dagligFast

--- L05_Case_Git/src/ordination/PN.java
+++ L05_Case_Git/src/ordination/PN.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class PN {
     private double antalEnheder;
+    private Ordination ordination;
 
     /**
      * Registrer, at der er givet en dosis paa dagen givesDen.
@@ -11,7 +12,11 @@ public class PN {
      * Returner false ellers, og datoen givesDen ignoreres.
      */
     public boolean givDosis(LocalDate givesDen) {
-
+        boolean IndenForGyldighed = false;
+        while (!IndenForGyldighed)
+            if (givesDen.isBefore(ordination.getSlutDen())&&givesDen.isAfter(ordination.getStartDen())){
+                IndenForGyldighed = true;
+            }
         return false;
     }
 
