a9a205ece552e7cbb8a91897a041ed0299fc18a3
Damse96 <eaaankd@students.eaaa.dk>
2022-09-08 10:37:28 +0200

    update

--- L05_Case_Git/Test/controller/ControllerTest.java
+++ L05_Case_Git/Test/controller/ControllerTest.java
@@ -38,27 +38,9 @@ class ControllerTest {
     jane, paracetamol, 123);
 
 
-
-    @org.junit.jupiter.api.Test
-    void opretDagligFastOrdination() {
-
-        Controller.opretPNOrdination(LocalDate.parse("2019-01-01"), LocalDate.parse("2019-01-12"),
-               jane, paracetamol, 123);
-
-        Controller.opretPNOrdination(LocalDate.parse("2019-02-12"), LocalDate.parse("2019-02-14"),
-                jane, acetylsalicylsyre, 3);
-
-        Controller.opretPNOrdination(LocalDate.parse("2019-01-20"), LocalDate.parse("2019-01-25"),
-                ib, fucidin, 5);
-
-        Controller.opretPNOrdination(LocalDate.parse("2019-01-01"), LocalDate.parse("2019-01-12"),
-                jane, paracetamol, 123);
-
-
-    }
-
     @org.junit.jupiter.api.Test
     void opretDagligSkaevOrdination() {
+
     }
 
     @org.junit.jupiter.api.Test
@@ -73,32 +55,9 @@ class ControllerTest {
     void antalOrdinationerPrVaegtPrLaegemiddel() {
     }
 
-    @org.junit.jupiter.api.Test
-    void getAllPatienter() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getAllLaegemidler() {
-    }
 
 
-    @org.junit.jupiter.api.Test
-    void opretLaegemiddel() {
-        Laegemiddel acetylsalicylsyre = Controller.opretLaegemiddel(
-                "Acetylsalicylsyre", 0.1, 0.15,
-                0.16, "Styk");
-        Laegemiddel paracetamol = Controller.opretLaegemiddel(
-                "Paracetamol", 1, 1.5,
-                2, "Ml");
-        Laegemiddel fucidin = Controller.opretLaegemiddel(
-                "Fucidin", 0.025, 0.025,
-                0.025, "Styk");
-       Laegemiddel methotrexate = Controller.opretLaegemiddel(
-                "Methotrexate", 0.01, 0.015,
-                0.02, "Styk");
-    }
 
-    @org.junit.jupiter.api.Test
-    void initStorage() {
-    }
+
+
 }
