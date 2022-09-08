14e58d0b723d6cb08c513e5f734517b19422d1bd
Puffbirbb <eaagmhc@students.eaaa.dk>
2022-09-08 12:01:53 +0200

    PN test

--- L05_Case_Git/Test/ordination/PNTest.java
+++ L05_Case_Git/Test/ordination/PNTest.java
@@ -2,31 +2,67 @@ package ordination;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PNTest {
 
     @Test
-    void givDosis() {
+    void givDosisFalse() {
+        //arrange
+        PN pn = new PN(LocalDate.of(2022,7,2),
+                LocalDate.of(2022, 7, 5),2);
+        //act
+        boolean actualResult = pn.givDosis(LocalDate.of(2022,7,6));
+        //assert
+        assertFalse(actualResult);
     }
 
     @Test
-    void getAntalGangeGivet() {
+    void givDosisTrue() {
+        //arrange
+        PN pn = new PN(LocalDate.of(2022,7,2),
+                LocalDate.of(2022, 7, 5),2);
+        //act
+        boolean actualResult = pn.givDosis(LocalDate.of(2022,7,4));
+        //assert
+        assertTrue(actualResult);
+    }
+
+
+    @Test
+    void samletDosisDage() {
+        //arrange
+        PN pn = new PN(LocalDate.of(2022,7,2),
+                LocalDate.of(2022, 7, 5),2);
+        //act
+        double actualResult = pn.samletDosis();
+        //assert
+        assertEquals(6.0, actualResult);
     }
 
     @Test
-    void samletDosis() {
+    void samletDosisMåned() {
+        //arrange
+        PN pn = new PN(LocalDate.of(2022,7,2),
+                LocalDate.of(2022, 8, 2),2);
+        //act
+        double actualResult = pn.samletDosis();
+        //assert
+        assertEquals(62, actualResult);
     }
 
     @Test
-    void doegnDosis() {
+    void samletDosisÅr() {
+        //arrange
+        PN pn = new PN(LocalDate.of(2022,7,2),
+                LocalDate.of(2023, 7, 2),2);
+        //act
+        double actualResult = pn.samletDosis();
+        //assert
+        assertEquals(730, actualResult);
     }
 
-    @Test
-    void getType() {
-    }
 
-    @Test
-    void getAntalEnheder() {
-    }
 }
